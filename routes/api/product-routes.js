fc650ae189195ae225f3419b4517907023e3be1c
Thomas Tran <tranthom618@gmail.com>
2023-03-15 22:17:04 -0400

    product-routes Completed - Delete route updated

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -45,7 +45,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({message: 'Could not find product with that ID'})
+      res.status(404).json({message: 'No product found with this id!'})
     }
 
     res.status(200).json(productData);
@@ -128,8 +128,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
